37581d1a29c53406d1c36489890c0b763f51a450
AlexHlukhyi <AlexHlukhyi@users.noreply.github.com>
2020-05-22 00:57:57 +0300

    v1.0 deployment

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -17,9 +17,6 @@
     <script>var csrfToken = '{{ csrf_token() }}'; var urlRoute = {};</script>
 </head>
 <body>
-    <div class="container">
-        <p style="color: red">Конфігуратор у розробці! Вибачте за технічні незручності!</p>
-    </div>
     <header>
         <hr/>
         @yield("page-header")
